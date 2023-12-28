9cb75a82aef6ed86efe366002a626c0197da7b35
= <=>
2023-12-28 23:14:46 +0530

    completed first video

--- project/api/urls.py
+++ project/api/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.api_overview, name="base"),
-
-    ]
+    path("task-list/", views.tasklist, name="task-list"),
+    path("create-task/", views.postView, name="post-task"),
+    path("task-list/<int:pk>", views.detailView, name="detail-view"),
+    path("update-task/<int:pk>", views.updateView, name="update-detail"),
+    path("delete-task/<int:pk>", views.deleteTask, name="delete-task"),
+]
--- project/api/views.py
+++ project/api/views.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializer import TaskSerializer
 from .models import Task
+from django.shortcuts import redirect
+
 
 # Create your views here.
 @api_view(["GET"])
@@ -16,6 +18,40 @@ def api_overview(request):
 
     return Response(api_urls)
 
+
+@api_view(["GET"])
 def tasklist(request):
     all_task = Task.objects.all()
-    serializer = TaskSerializer()
+    serializer = TaskSerializer(all_task, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def detailView(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def postView(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(["PATCH"])
+def updateView(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(["DELETE"])
+def deleteTask(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return redirect("task-list")
