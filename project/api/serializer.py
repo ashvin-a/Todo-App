290672887a250cc6ba674fc7f7672449e1f6e06c
Ashvinrockz <88977094+ashvin-a@users.noreply.github.com>
2024-07-07 17:16:41 +0530

    add a comment for serializer.

--- project/api/serializer.py
+++ project/api/serializer.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import Task
 
-
+"""
+Class for serializer
+"""
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
